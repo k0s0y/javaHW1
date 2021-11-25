24405e9922cea86330f2ac62400eb5323b4e4350
Timofei <t.a.maksimenko@ya.ru>
2021-11-25 10:54:44 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int currentBalance = 2_000_000_000;
+        int updateBalance = 500_000_000;
+        int newBalance = currentBalance * updateBalance;
+        System.out.println(newBalance);
+    }
+}
